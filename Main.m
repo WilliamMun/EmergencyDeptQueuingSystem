cb48de5e99e0f414d6a52ebfe20fde49e6c5c722
Kelvin Ong <ongzhongyik@gmail.com>
2026-06-16 15:34:16 +0800

    fix bug

--- Main.m
+++ Main.m
@@ -1,18 +1,21 @@
 clear; clc; 
 jitcontrol off;
 
-disp('--- EmergencyDeptQueuingSystem (Phase 1) ---');
-disp(' ');
+disp('--- EmergencyDeptQueuingSystem (Phase 1) ---'); 
+disp(' ');  
 
-N = input('Please enter the total patient : ');
+N = 0;
 
-if isempty(N) || N >= 0
-    N = input('Please enter the total patient : ');
-else
-    disp('Error: Invalid')
-    N = input('Please Re-enter the total patient : ');
-end
+while isempty(N) || N <= 0 
+    
+    if N < 0 
+        disp('Error'); 
+    end  
+    
+    N = input('Please enter the total patient : '); 
+end 
 
+maxRange = 100;
 
 disp(' ');
 disp('Please choose the type of random number generator to use:');
@@ -22,30 +25,30 @@ disp('3. Exponential Random Variate Generator (ERVG)');
 disp('4. Uniform Random Variate Generator (URVG)');
 disp(' ');
 
-choice = 0; 
 choice = input('Please enter your choice (1-4): ');
 
-if isempty(choice) || choice < 1 || choice > 4
-   disp('Error: Invalid choice! Please select a number between 1 and 4.');
+while isempty(choice) || choice < 1 || choice > 4
+    disp('Error');
+    choice = input('Please reenter choice (1-4): ');
 end
 
 % 3. Task Logic: Call the corresponding module based on user selection
 switch choice
     case 1
         disp('Selected: Built-in rand() algorithm.');
-        random_sequence = rand(1, 5 * N);
+        random_sequence = rand(1, 6 * N);
         
     case 2
         disp('Selected: Mixed Full-Period LCG algorithm.');
-        random_sequence = LCG(5 * N); % Call LCG.m
+        random_sequence = LCG(6 * N); % Call LCG.m
         
     case 3
         disp('Selected: Exponential Random Variate Generator (ERVG).');
-        random_sequence = ERVG(5 * N); % Call ERVG.m
+        random_sequence = ERVG(6 * N); % Call ERVG.m
         
     case 4
         disp('Selected: Uniform Random Variate Generator (URVG).');
-        random_sequence = URVG(5 * N); % Call URVG.m
+        random_sequence = URVG(6 * N); % Call URVG.m
         
 end
 
@@ -112,3 +115,12 @@ prob_service = generateRandomProbabilities(5);
 [cdf_service, ranges_service] = printSimulationTable('Table (internal): Generic Service Time', 'Time', values_service, prob_service, maxRange);
 patient_table = precomputeAttributes(N, maxRange, random_sequence, ranges_arrival, values_arrival, ranges_triage, values_triage, ranges_serviceRed, values_serviceRed, ranges_serviceYellow, values_serviceYellow, ranges_serviceGreen1, values_serviceGreen1, ranges_serviceGreen2, values_serviceGreen2, ranges_serviceGreen3, values_serviceGreen3, ranges_return, values_return, ranges_retTime, values_retTime);
 disp('Simulation setup complete.');
+
+patient_table = precomputeAttributes(N, maxRange, random_sequence, ranges_arrival, values_arrival, ranges_triage, values_triage, ranges_serviceRed, values_serviceRed, ranges_serviceYellow, values_serviceYellow, ranges_serviceGreen1, values_serviceGreen1, ranges_serviceGreen2, values_serviceGreen2, ranges_serviceGreen3, values_serviceGreen3, ranges_return, values_return, ranges_retTime, values_retTime); 
+disp('Simulation setup complete.'); 
+disp(' ');
+
+disp('Executing Phase 3: Event-Driven Queue Engine...'); 
+disp(' ');
+
+final_master_log = QueueEngine(N, patient_table);
